fb815f123aeaca901722d89ec41c7081d7246bac
Samio Santos <71763791+Samio-Santos@users.noreply.github.com>
2021-08-10 08:48:04 -0300

    Delete archive.py

--- archive.py
+++
@@ -1,8 +0,0 @@
-# Email
-EMAIL = 'samio95.santos@gmail.com'
-PASSWORD = 'samy4667'
-
-# Autenticação com redes sociais
-SECRET_KEY_FACEBOOK = 'd68fed2d64092e2cb32bc5fadf800682'
-SECRET_KEY_GITHUB = '35585559b09ce930e09bfa9cf1327ae469766ff4'
-SECRET_KEY_GOOGLE = 'EOQ-7TD62FBMBmNfdwqulvgy'
