d0cdf9d20797514f7586efc8593c0d6dc50e29cf
Samio-Santos <samy.santos@hotmail.com>
2025-03-28 03:36:44 -0300

    CI/CD V9

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,8 @@
 # Para o script em caso de erro
 set -e  
 
-# Verifica se há alterações locais no deploy.sh e realiza o commit ou stash, se necessário
-
-# Se o deploy.sh foi alterado localmente, comita ou faz o stash das alterações
-git diff --exit-code deploy.sh || {
-    echo "Alterações locais encontradas em deploy.sh, commit ou stash necessário."
-
-    # Comitar as mudanças no deploy.sh
-    git add deploy.sh
-    git commit -m "Alterações locais em deploy.sh antes do pull"
-
-    # Alternativa: se preferir guardar temporariamente as alterações
-    # git stash
-}
+# Desfaz alterações locais no deploy.sh
+git checkout -- deploy.sh
 
 # Atualiza o código
 git pull origin main
@@ -26,6 +15,7 @@ docker-compose build
 docker-compose up -d
 
 # Aplica migrações dentro do container Django
+docker exec -it sistema_container python manage.py makemigrations
 docker exec -it sistema_container python manage.py migrate
 
 # Coleta arquivos estáticos
